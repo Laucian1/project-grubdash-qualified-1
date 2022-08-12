6d7df4654a1721ba180099c6beb3415aa49ecf7b
Russell Smith <resmithjr88@gmail.com>
2022-08-11 20:33:51 -0600

    Dishes controller fleshed out

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,4 +6,110 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res) {
+    res.json({ data: dishes })
+}
+
+function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+        const { data = {} } = req.body
+        if (data[propertyName]) {
+            return next()
+        }
+        next({
+            status: 400,
+            message: `Must include a ${propertyName}`
+        })
+    }
+}
+
+function priceIsValidNumber(req, res, next) {
+    const { data: { price } = {} } = req.body
+    if (price <= 0 || !Number.isInteger(price)) {
+        return next({
+            status: 400,
+            message: "Dish must have a price that is an integer greater than 0"
+        })
+    }
+    next()
+}
+
+function create(req, res) {
+    const { data: { name, description, price, image_url } = {} } = req.body
+    const newDish = {
+        id: nextId,
+        name,
+        description,
+        price,
+        image_url,
+    }
+    dishes.push(newDish)
+    res.status(201).json({ data: newDish })
+}
+
+function dishExists(req, res, next) {
+    const { dishId } = req.params
+    const foundDish = dishes.find((dish) => dish.id === dishId)
+    if (foundDish) {
+        res.locals.dish = foundDish
+        return next()
+    }
+    next({
+        status: 404,
+        message: `Dish does not exist: ${dishId}`
+    })
+}
+
+function read(req, res) {
+    res.json({ data: res.locals.paste })
+}
+
+function idMatch(req, res, next) {
+    const { dishId } = req.params
+    const { data: { id } = {} } = req.body
+    if (id === dishId || !id) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`
+    })
+}
+
+function update(req, res) {
+    const dish = res.locals.dish
+    const { data: { name, description, price, image_url } = {} } = req.body
+
+    dish.name = name
+    dish.description = description
+    dish.price = price
+    dish.image_url = image_url
+
+    res.json({ data: dish })
+}
+
+module.exports = {
+    list,
+    create: [
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValidNumber,
+        create,
+    ],
+    read: [
+        dishExists, 
+        read,
+    ],
+    update: [
+        dishExists,
+        idMatch,
+        bodyDataHas("name"),
+        bodyDataHas("description"),
+        bodyDataHas("price"),
+        bodyDataHas("image_url"),
+        priceIsValidNumber,
+        update,
+    ],
+}
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,4 +7,12 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+function list(req, res) {
+    const { dishId } = req.params
+    res.json({ data: orders
+        .filter(
+            dishId ? order => order.dishes.id == dishId 
+            : () => true
+        ) 
+    })
+}
